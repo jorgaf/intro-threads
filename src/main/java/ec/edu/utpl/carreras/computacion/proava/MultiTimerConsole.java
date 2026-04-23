e894251cc8e696442912337c30cce09fe9c10c49
diegolojan12 <dslojan1@utpl.edu.ec>
2026-04-23 09:14:10 -0500

    complete

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -4,9 +4,25 @@ package ec.edu.utpl.carreras.computacion.proava;
  * Hello world!
  */
 public class MultiTimerConsole {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         TimerTask timerA = new TimerTask("Temporizador A", 8);
-        timerA.run();
-        System.out.println();
+        TimerTask timerB = new TimerTask("Temporizador B", 5);
+        TimerTask timerC = new TimerTask("Temporizador C", 3);
+
+        Thread hiloA = new Thread(() -> timerA.run(), "Hilo-A");
+        Thread hiloB = new Thread(() -> timerB.run(), "Hilo-B");
+        Thread hiloC = new Thread(() -> timerC.run(), "Hilo-C");
+
+        hiloA.start(); hiloB.start(); hiloC.start();
+
+        try {
+            hiloA.join();
+            hiloB.join();
+            hiloC.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println(">>> Todos los temporizadores han finalizado.");
     }
 }
--- src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
@@ -9,10 +9,18 @@ public class TimerTask {
         this.seconds = seconds;
     }
 
+
     public void run() {
-        while(seconds > 0) {
+        while (seconds > 0) {
             System.out.printf("%s: %d segundos restantes%n", name, seconds);
-            seconds --;
+            seconds--;
+            try {
+                Thread.sleep(1000);  // pausa 1 segundo real
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
         }
+        System.out.printf("%s: ¡Tiempo!%n", name);
     }
 }
