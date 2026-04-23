606f939edd5e56c1059edc2596951cb603ab0da5
Jorandres13 <jaguerrero39@utpl.edu.ec>
2026-04-23 08:27:06 -0500

    Parte 1: TimerTask extiende Thread

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
@@ -1,14 +1,16 @@
 package ec.edu.utpl.carreras.computacion.proava;
 
-public class TimerTask {
+public class TimerTask extends Thread {
     private final String name;
     private int seconds;
 
     public TimerTask(String name, int seconds) {
+        super(name);
         this.name = name;
         this.seconds = seconds;
     }
 
+    @Override
     public void run() {
         while(seconds > 0) {
             System.out.printf("%s: %d segundos restantes%n", name, seconds);
