feaab106aaddc08741360ab2cb1c34cc968ba163
Eimer05 <147772352+Eimer05@users.noreply.github.com>
2026-04-23 11:17:04 -0500

    solucion-lambda

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -6,7 +6,13 @@ package ec.edu.utpl.carreras.computacion.proava;
 public class MultiTimerConsole {
     public static void main(String[] args) {
         TimerTask timerA = new TimerTask("Temporizador A", 8);
-        timerA.run();
-        System.out.println();
+        TimerTask timerB = new TimerTask("Temporizador B", 5);
+        TimerTask timerC = new TimerTask("Temporizador C", 3);
+
+        new Thread(() -> timerA.run(), "Hilo-A").start();
+        new Thread(() -> timerB.run(), "Hilo-B").start();
+        new Thread(() -> timerC.run(), "Hilo-C").start();
+
+        new Thread(timerA::run, "Hilo-A").start();
     }
 }
