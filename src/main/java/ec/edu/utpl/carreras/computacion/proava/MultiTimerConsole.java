29a97accb1ee9ede8036cb41a1bbaa8203b7cf8c
diegolojan12 <dslojan1@utpl.edu.ec>
2026-04-23 09:39:20 -0500

    complete

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -22,7 +22,6 @@ public class MultiTimerConsole {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-
         System.out.println(">>> Todos los temporizadores han finalizado.");
     }
 }
