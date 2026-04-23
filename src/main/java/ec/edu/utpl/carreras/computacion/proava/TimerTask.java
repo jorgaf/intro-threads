a17bb34904b5eed6056150006da51d103c2a63d9
Eimer05 <147772352+Eimer05@users.noreply.github.com>
2026-04-23 11:36:43 -0500

    solucion-join-sleep

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
@@ -10,9 +10,16 @@ public class TimerTask {
     }
 
     public void run() {
-        while(seconds > 0) {
+        while (seconds > 0) {
             System.out.printf("%s: %d segundos restantes%n", name, seconds);
-            seconds --;
+            seconds--;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
         }
+        System.out.printf("%s: ¡Tiempo!%n", name);
     }
 }
